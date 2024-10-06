38f054c5e74d9d6065f8d5518cce0a979f831ed4
wzz <ywc0008@naver.com>
2024-10-06 23:23:11 +0900

    temporary

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -26,6 +27,7 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
+        <Header />
         {children}
       </body>
     </html>
---
+++ front/components/Header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <div>
+      <div>북적북적</div>
+      <nav>
+        <ul>
+          <li></li>
+        </ul>
+      </nav>
+      <div>로그인 정보</div>
+    </div>
+  );
+}
